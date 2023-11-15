8e55ed15d6acb3fecf11bcb9805705a4aa06dae6
glenjh <51897470+glenjh@users.noreply.github.com>
2023-11-15 21:00:59 +0900

    [Silver IV] Title: 크림 파스타, Time: 48 ms, Memory: 3460 KB -BaekjoonHub

--- 백준/Silver/25214. 크림 파스타/크림 파스타.cc
+++ 백준/Silver/25214. 크림 파스타/크림 파스타.cc
@@ -14,37 +14,21 @@ int dp[MAX];
 int main() {
     FastIO();
 
-    int n, mm = 0 ,MM = 0, localSub;
+    int n, mm = 0, minVal;
     int arr[MAX];
     cin >> n;
     for(int i = 0 ; i < n ; i++){
-        int temp1 = 0, temp2 = 0, temp3;
         cin >> arr[i];
         
         if(i == 0){
             dp[i] = 0;
-            mm = arr[i];
-            MM = arr[i];
+            minVal = arr[i];
         }
         else{
-            if(arr[i] < mm){
-                mm = arr[i];
-                dp[i] = dp[i - 1];
+            if(arr[i] < minVal){
+                minVal = arr[i];
             }
-            else{
-                if(arr[i] > MM){
-                    temp1 = arr[i] - MM;
-                    MM = arr[i];
-                }
-                temp2 = arr[i] - mm;
-                temp3 = max(temp1, temp2);
-                if(temp3 < dp[i - 1]){
-                    dp[i] = dp[i - 1];
-                }
-                else{
-                    dp[i] = temp3;
-                }
-            }
+            dp[i] = max(dp[i - 1], arr[i] - minVal);
         }
     }
 
