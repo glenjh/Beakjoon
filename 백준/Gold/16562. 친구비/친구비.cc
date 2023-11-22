e0d33b4dad939c584f323ad05d439a0e8d2c611e
glenjh <51897470+glenjh@users.noreply.github.com>
2023-11-22 14:26:13 +0900

    [Gold IV] Title: 친구비, Time: 4 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.cc
+++ 백준/Gold/16562. 친구비/친구비.cc
@@ -11,8 +11,8 @@ void FastIO(){
 }
 
 int p[MAX];
-int nodeCnt[MAX];
-int temp[MAX];
+int cost[MAX];
+bool visited[MAX];
 
 int My_Find(int a){
     if(a == p[a]){ return a; }
@@ -24,44 +24,36 @@ void My_Uni(int a, int b){
     int pb = My_Find(b);
     if(pa == pb){ return; }
 
-    if(nodeCnt[pa] > nodeCnt[pb]){
-        p[pb] = pa;
-        nodeCnt[pa] += nodeCnt[pb];
+    if(cost[pa] > cost[pb]){
+        p[pa] = pb;
     }
     else{
-        p[pa] = pb;
-        nodeCnt[pb] += nodeCnt[pa];
+        p[pb] = pa;
     }
-
-    p[pa] = pb;
 }
 
 int main() {
     FastIO();
 
-    int cost[MAX];
     int n, m, k;
     cin >> n >> m >> k;
     for(int i = 1 ; i <= n ; i++){
-        temp[i] = INT_MAX;
         p[i] = i;
         cin >> cost[i];
     }
+
     for(int i = 0 ; i < m ; i++){
         int a, b;
         cin >> a >> b;
         My_Uni(a, b);
     }
 
-    for(int i = 1 ; i <= n ; i++){
-        int root = My_Find(i);
-        temp[root] = min(temp[root], cost[i]);
-    }
-
     int answer = 0;
     for(int i = 1 ; i <= n ; i++){
-        if(temp[i] != INT_MAX){
-            answer += temp[i];
+        int root = My_Find(i);
+        if(!visited[root]){
+            visited[root] = true;
+            answer += cost[root];
         }
     }
 
