740353e2ea7a6d347dcebc8777f81bced2955205
glenjh <51897470+glenjh@users.noreply.github.com>
2023-12-01 18:12:25 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 348 ms, Memory: 20976 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-void FastIO(){
+void FastIO()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
@@ -13,41 +14,56 @@ void FastIO(){
 
 int p[MAX];
 
-int My_Find(int a){
-    if(a == p[a]){ return a; }
+int My_Find(int a)
+{
+    if (a == p[a])
+    {
+        return a;
+    }
     return p[a] = My_Find(p[a]);
 }
 
-void My_Union(int a, int b){
+void My_Union(int a, int b)
+{
     int pa = My_Find(a);
     int pb = My_Find(b);
 
-    if(pa == pb){ return; }
+    if (pa == pb)
+    {
+        return;
+    }
 
-    if(pa > pb){
+    if (pa > pb)
+    {
         p[pa] = pb;
-    }else{
+    }
+    else
+    {
         p[pb] = pa;
     }
 }
 
-bool comp(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b){
+bool comp(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b)
+{
     return a.first < b.first;
 }
 
-int main() {
-    FastIO();  
+int main()
+{
+    FastIO();
 
-    vector< pair<int, pair<int, int>> > v;
+    vector<pair<int, pair<int, int>>> v;
     vector<int> rel;
     int n, m;
     int answer = 0;
     cin >> n >> m;
-    for(int i = 1 ; i <= n ; i++){
+    for (int i = 1; i <= n; i++)
+    {
         p[i] = i;
     }
 
-    for(int i = 0 ; i < m ; i++){
+    for (int i = 0; i < m; i++)
+    {
         int a, b, c;
         cin >> a >> b >> c;
         v.push_back({c, {a, b}});
@@ -55,18 +71,21 @@ int main() {
 
     sort(v.begin(), v.end(), comp);
 
-    for(int i = 0 ; i < v.size() ; i++){
+    for (int i = 0; i < v.size(); i++)
+    {
         int a = v[i].second.first, b = v[i].second.second;
 
         int pa = My_Find(a);
         int pb = My_Find(b);
-        if(pa != pb){
+        if (pa != pb)
+        {
             My_Union(a, b);
             rel.push_back(v[i].first);
         }
     }
 
-    for(int i = 0 ; i < rel.size() - 1 ; i++){
+    for (int i = 0; i < rel.size() - 1; i++)
+    {
         answer += rel[i];
     }
 
