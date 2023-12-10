9e9a491cb848b069255e40cd0aba1624a6a1bbda
glenjh <51897470+glenjh@users.noreply.github.com>
2023-12-10 21:59:01 +0900

    [Silver V] Title: 귀찮음, Time: 0 ms, Memory: 2024 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void FastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main() {
+    FastIO();
+
+    vector<int> v;
+    int n, answer = 0, sum = 0;
+    cin >> n;
+    for(int i = n ; i >= 1 ; i--){
+        int input;
+        cin >> input;
+        sum += input;
+        v.push_back(input);
+    }
+    sort(v.begin(), v.end());
+
+    for(int i = 0 ; i < n ; i++){
+        sum = sum - v[i];
+        answer += sum * v[i];
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
