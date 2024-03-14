75b8776f48915dcd435ffee9ff4bce10f43068e3
glenjh <51897470+glenjh@users.noreply.github.com>
2024-03-14 16:49:12 +0900

    [Silver III] Title: 천재 수학자 성필, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+void FastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    string formula;
+    getline(cin, formula);
+
+    stack<int> s;
+    for (int i = 0; i < formula.length(); i++)
+    {
+        char c = formula[i];
+
+        if (c >= '0' && c <= '9')
+        {
+            s.push(c - '0');
+        }
+        else
+        {
+            int a, b;
+            a = s.top();
+            s.pop();
+            b = s.top();
+            s.pop();
+
+            if (c == '+')
+            {
+                s.push(b + a);
+            }
+            else if (c == '-')
+            {
+                s.push(b - a);
+            }
+            else if (c == '*')
+            {
+                s.push(b * a);
+            }
+            else if (c == '/')
+            {
+                s.push(b / a);
+            }
+        }
+    }
+
+    cout << s.top();
+}
