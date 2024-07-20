50211930d972a8091e77ef4d2f464538550e7cd8
glenjh <51897470+glenjh@users.noreply.github.com>
2024-07-20 20:27:37 +0900

    [Silver I] Title: 부동산 다툼, Time: 52 ms, Memory: 5352 KB -BaekjoonHub

--- 백준/Silver/20364. 부동산 다툼/부동산 다툼.cc
+++ 백준/Silver/20364. 부동산 다툼/부동산 다툼.cc
@@ -12,68 +12,39 @@ void FastIO(){
 
 ll n, q;
 vector<bool> visited;
-vector<ll> wantList;
 vector<ll> p;
-
-void Sol(ll a, ll origin){
-    ll block;
-    bool flag = true;
-
-    while(p[a] != 1){
-        a = p[a];
-
-        if (visited[a]){
-            block = a;
-            flag = false;
-            // break;
-        }
-    }
-
-    if(flag){
-        visited[origin] = true;
-        cout << 0 << '\n';
-    }else{
-        cout << block << '\n';
-    }
-}
 
 int main() {
     FastIO();
 
     cin >> n >> q;
     for(ll i = 0 ; i <= n ; i++){
-        // p.push_back(i + 1);
         visited.push_back(false);
     }
 
     for(ll i = 0 ; i < q ; i++){
         ll input, answer = 0;
+        bool flag = true;
         cin >> input;
-        // wantList.push_back(input);
 
         int temp = input;
 
         while(temp > 1){
             if(visited[temp]){
+                flag = false;
                 answer = temp;
             }
             temp /= 2;
         }
-        visited[input] = true;
+        if(flag){
+            visited[input] = true;
+        }
         p.push_back(answer);
     }
 
     for(auto it : p){
         cout << it << '\n';
     }
-
-    // for(ll i = 1 ; i <= n ; i++){
-    //     p[i] = i / 2;
-    // }
-
-    // for(ll i = 0 ; i < q ; i++){
-    //     Sol(wantList[i], wantList[i]);
-    // }
 
     return 0;
 }
