a24aad3ae39d273606dbe3e5d7107addbee86289
glenjh <51897470+glenjh@users.noreply.github.com>
2024-07-23 13:18:53 +0900

    [Silver II] Title: 차이를 최대로, Time: 4 ms, Memory: 2536 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void FastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int answer = 0;
+bool visited[9];
+vector<int> v;
+vector<int> ans;
+vector<int> lists;
+
+void DFS(int depth, int n){
+    if(depth == n){
+        int temp = 0;
+        for(int i = 0 ; i < n - 1 ; i++){
+            temp += abs(lists[i] - lists[i + 1]);
+        }
+        ans.push_back(temp);
+
+        return;
+    }
+
+    for(int i = 0 ; i < v.size() ; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            lists.push_back(v[i]);
+
+            DFS(depth + 1, n);
+
+            lists.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+int main() {
+    FastIO();
+
+    int n, answer = 0;
+    cin >> n;
+
+    for(int i = 0 ; i < n ; i++){
+        int input;
+        cin >> input;
+        v.push_back(input);
+    }
+    sort(v.begin(), v.end());
+
+    DFS(0, n);
+
+    sort(ans.begin(), ans.end());
+    cout << ans[ans.size() - 1] << '\n';
+
+    return 0;
+}
