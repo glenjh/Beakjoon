a7454021edd1c96ed6f23a30bbef861cf92e259c
glenjh <51897470+glenjh@users.noreply.github.com>
2024-09-17 11:35:54 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 88 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -10,7 +10,7 @@ void FastIO(){
     cout.tie(NULL);
 }
 
-
+ll dp[1000001];
 
 int main() {
     FastIO();
@@ -19,21 +19,18 @@ int main() {
     cin >> testCase;
 
     for(int t = 0 ; t < testCase ; t++){
-        ll dp[1000001];
         int n;
         cin >> n;
-
-        for(ll i = 1 ; i <= n ; i++){
-            dp[i] = 0;
-        }
 
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 4;
 
         for(ll i = 4 ; i <= n ; i++){
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            dp[i] %= mod;
+            if(dp[i] == 0){
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+                dp[i] %= mod;
+            }
         }
 
         cout << dp[n] << '\n';
