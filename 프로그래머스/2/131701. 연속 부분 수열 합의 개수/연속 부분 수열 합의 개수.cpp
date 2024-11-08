7e290f8eb2e5425a714ed25b3483b6fa5d39ea1b
glenjh <51897470+glenjh@users.noreply.github.com>
2024-11-08 21:03:53 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 395.02 ms, Memory: 22.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    set<int> s;
+    
+    for(int i = 0 ; i < elements.size() ; i++){
+        int sum = 0;
+        for(int j = i ; j < i + elements.size() ; j++){
+            sum += elements[j % elements.size()];
+            s.insert(sum);
+        }
+    }
+    answer = s.size();
+    
+    
+    return answer;
+}
